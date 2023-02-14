d24e2efc0b8f4edeab3e7d6364d415b42b5664de
lishijiee <2424782003@qq.com>
2023-02-14 19:38:18 +0800

    Initial commit

---
+++ src/test/java/io/lishjiee/boot/SpringBootNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lishjiee.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootNotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
