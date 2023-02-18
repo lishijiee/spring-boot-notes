e482b88ce503c1b42832194fe7b734384fe9ba89
lishijiee <2424782003@qq.com>
2023-02-18 21:51:06 +0800

    整合spring-security

---
+++ spring-boot-security/src/test/java/io/lishijiee/boot/SpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lishijiee.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
