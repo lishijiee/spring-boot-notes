e06fad57a6c8254f4594bd97fc8f6ef2e06a0e20
lishijiee <2424782003@qq.com>
2023-02-27 08:10:23 +0800

    添加模块

--- spring-boot-mybatis/src/main/java/io/lishijiee/boot/controller/AcctController.java
+++ spring-boot-mybatis/src/main/java/io/lishijiee/boot/controller/AcctController.java
@@ -3,9 +3,11 @@ package io.lishijiee.boot.controller;
 import io.lishijiee.boot.pojo.AcctInf;
 import io.lishijiee.boot.services.AcctServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -14,6 +16,7 @@ public class AcctController {
     AcctServices acctService;
     @RequestMapping("/getAll")
     public List<AcctInf> getAllAcct(){
+        List list = new ArrayList();
         return acctService.getAllAcct();
     }
 }
